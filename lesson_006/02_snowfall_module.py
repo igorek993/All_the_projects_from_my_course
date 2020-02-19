ad6ca82557e54c3136922cc80a459ae8e4b52245
Igor Zhukov <igorek993@gmail.com>
2020-02-19 22:04:00 +1100

    lesson 6 task 2 I am confused with the actual task description

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -8,19 +8,34 @@ import simple_draw as sd
 #  нарисовать_снежинки_цветом(color) - отрисовывает все снежинки цветом color
 #  сдвинуть_снежинки() - сдвигает снежинки на один шаг
 #  номера_достигших_низа_экрана() - выдает список номеров снежинок, которые вышли за границу экрана
-#  удалить_снежинки(номера) - удаляет снежинки с номерами из списка
+#  удалить_снежинки(номера) - удаляет снежинки с номерами из списка TODO I cant understand why I should create
+#   todo 'список номеров снежинок' then show them on the screen and then delete them all from the same list???
+# todo I can't also understand the point of making a list of escaped snowflakes and showing it in the console :)
+# todo To make a long story short, I do not quite understand the last steps as they do not make sense to me....
+# todo Would you be able to explain to me what I have to do a bit more clear? thank you!
 #
 # В текущем модуле реализовать главный цикл падения снежинок,
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
 # создать_снежинки(N)
+# while True:
+#  нарисовать_снежинки_цветом(color=sd.background_color)
+#  сдвинуть_снежинки()
+#  нарисовать_снежинки_цветом(color)
+#  если есть номера_достигших_низа_экрана() то
+#       удалить_снежинки(номера)
+#       создать_снежинки(count)
+
+
+from lesson_006.snowfall import create_snowflakes, draw_colored_snowflakes, move_snowflakes, \
+    draw_background_snowflakes, escaped_snowflakes
+
+create_snowflakes(20)
 while True:
-    #  нарисовать_снежинки_цветом(color=sd.background_color)
-    #  сдвинуть_снежинки()
-    #  нарисовать_снежинки_цветом(color)
-    #  если есть номера_достигших_низа_экрана() то
-    #       удалить_снежинки(номера)
-    #       создать_снежинки(count)
+    draw_background_snowflakes()
+    move_snowflakes()
+    draw_colored_snowflakes(sd.COLOR_YELLOW)
+    escaped_snowflakes()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
---
+++ lesson_006/snowfall.py
@@ -0,0 +1,46 @@
+import simple_draw as sd
+
+x_list = []
+y_list = []
+flake_length_list = []
+sd.resolution = (1200, 900)
+
+
+def create_snowflakes(n):
+    global number
+    number = n
+    for _ in range(number):
+        x_list.append(sd.random_number(0, sd.resolution[0]))
+        y_list.append(sd.random_number(sd.resolution[1], sd.resolution[1] * 2))
+        flake_length_list.append(sd.random_number(20, 50))
+
+
+def draw_background_snowflakes():
+    sd.start_drawing()
+    for x, y, length in zip(x_list, y_list, flake_length_list):
+        sd.snowflake(sd.get_point(x, y), length, color=sd.background_color)
+
+
+def move_snowflakes():
+    for i in range(number):
+        x_list[i] += sd.random_number(-15, 15)
+        y_list[i] -= sd.random_number(4, 30)
+
+
+def draw_colored_snowflakes(color):
+    for x, y, length in zip(x_list, y_list, flake_length_list):
+        sd.snowflake(sd.get_point(x, y), length, color=color)
+    sd.finish_drawing()
+
+
+def escaped_snowflakes():
+    global number_escaped_snowflakes
+    number_escaped_snowflakes = []
+    for _ in range(number):
+        if y_list[_] <= 0:
+            number_escaped_snowflakes.append(_)
+            print(number_escaped_snowflakes)
+
+
+def delete_from_list():
+    pass # I do not understand why would I create a list just to delete the same variables from it?...
