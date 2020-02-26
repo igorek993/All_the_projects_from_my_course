7518a21403351868bece0f77e6d731f4e6c234d8
Igor Zhukov <igorek993@gmail.com>
2020-02-26 21:16:57 +1100

    lesson 7 task 2

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -16,10 +16,118 @@
 # Вывод элемента на консоль реализовывать через __str__
 #
 # Примеры преобразований:
-#   print(Water(), '+', Air(), '=', Water() + Air())
+# print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+    def __str__(self):
+        return 'water'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+
+
+class Air:
+    def __str__(self):
+        return 'air'
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Thunder()
+        elif isinstance(other, Earth):
+            return Dust()
+        elif isinstance(other, Water):
+            return Storm()
+
+
+class Fire:
+    def __str__(self):
+        return 'fire'
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Lava()
+        elif isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Thunder()
+
+
+class Earth:
+    def __str__(self):
+        return 'earth'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+
+
+class Storm:
+    def __str__(self):
+        return 'storm'
+
+    def __add__(self, other):
+        return None
+
+
+class Steam:
+    def __str__(self):
+        return 'steam'
+
+    def __add__(self, other):
+        return None
+
+
+class Dirt:
+    def __str__(self):
+        return 'dirt'
+
+    def __add__(self, other):
+        return None
+
+
+class Thunder:
+    def __str__(self):
+        return 'thunder'
+
+    def __add__(self, other):
+        return None
+
+
+class Dust:
+    def __str__(self):
+        return 'dust'
+
+    def __add__(self, other):
+        return None
+
+
+class Lava:
+    def __str__(self):
+        return 'lava'
+
+    def __add__(self, other):
+        return None
+
+
+class SmallCat:
+    def __str__(self):
+        return 'myau'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return 'wet kitty'
+        elif isinstance(other, Fire):
+            return 'no more myau'
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
