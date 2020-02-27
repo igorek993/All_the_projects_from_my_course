3914d2fd2da7dd340728550edb8d7f6d16bdea04
Alexander Olhovik <aao.t@yandex.ru>
2020-02-27 12:53:43 +0300

    урок 7, недочёты в 1, 2, 3

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -12,14 +12,21 @@ import simple_draw as sd
 class Snowflake:
 
     def __init__(self, number):
-        self.number = number
+        self.number = number  # TODO Нигде не используется, более того, при добавлении новых снежинок номера перестают
+                              #  быть уникальными. Тогда вопрос - зачем этот атрибут нужен?
         self.x = [sd.random_number(0, sd.resolution[0])]
         self.y = [sd.random_number(sd.resolution[1], sd.resolution[1] * 2)]
-        self.flake_length = [sd.random_number(20, 50)]
+        self.flake_length = [sd.random_number(20, 50)]  # TODO Повторяться в классе снежинка, что этот параметр
+                                                        #  относится к снежинке, избыточно. Просто length достаточно
+        # TODO Это класс снежинки. У снежинки какие параметры? Координаты и размер. Ей достаточно одного икс, одного
+        #  игрек и одного размера. Тогда зачем же тут списки? Они вам мешают, и вы с ними "боретесь" используя
+        #  индексацию, циклы с zip, но сохраняете списки :)
 
     def clear_previous_picture(self):
-        sd.start_drawing()
+        sd.start_drawing()  # TODO Это должно быть до главного цикла по снежинками в основном коде
+
         for x, y, length in zip(self.x, self.y, self.flake_length):
+            # TODO На деле всё значительно проще, цикл тут не нужен
             sd.snowflake(sd.get_point(x, y), length, color=sd.background_color)
 
     def move(self):
@@ -28,11 +35,13 @@ class Snowflake:
 
     def draw(self):
         for x, y, length in zip(self.x, self.y, self.flake_length):
+            # TODO На деле всё значительно проще, цикл тут не нужен
             sd.snowflake(sd.get_point(x, y), length, color=sd.COLOR_WHITE)
-        sd.finish_drawing()
+        sd.finish_drawing()  # TODO Это должно быть после главного цикла по снежинками в основном коде
 
     def can_fall(self):
         if self.y[0] <= self.flake_length[0]:
+            # TODO Просто поставьте условие в return
             return True
 
 
@@ -59,7 +68,7 @@ def get_fallen_flakes():
     global flakes
     count = 0
     for flake in flakes:
-        if flake.y[0] <= 0:
+        if flake.y[0] <= 0:  # TODO Примените готовый метод объекта снежинка can_fall
             count += 1
     return count
 
