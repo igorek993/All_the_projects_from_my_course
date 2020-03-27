bf43e54f4ec6f98436569981b8e4cf8d0c0c94ff
Igor Zhukov <igorek993@gmail.com>
2020-03-27 20:22:15 +1100

    lesson 9 task 3 extra task not done yet(creates subfolders)

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -3,7 +3,6 @@
 import os, time, shutil
 import zipfile as zp
 
-
 # Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
 # Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
 # Например, так:
@@ -36,33 +35,83 @@ import zipfile as zp
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
+ICONS_FOLDER = os.path.normpath('C:\\Users\igorek\PycharmProjects\python_base\lesson_009\icons.zip')
+
+FINAL_FOLDER = os.path.normpath('C:\\Users\igorek\PycharmProjects\python_base\lesson_009')
+
+
 class PhotosSorter:
+
+    def __init__(self, scanned_files_dir, dir_to_scan):
+        self.scanned_files_dir = scanned_files_dir
+        self.dir_to_scan = dir_to_scan
 
     def unzip_files(self, file):
         with zp.ZipFile(file, 'r') as file_z:
             file_z.extractall()
 
-    def sort_files(self, dir_to_scan, scanned_files_dir):
-        if not os.path.isdir(os.path.join(scanned_files_dir, 'icons_by_year')):
-            os.makedirs(os.path.join(scanned_files_dir, 'icons_by_year'))
-        for dirpath, dirnames, filenames in os.walk(dir_to_scan):
+    def create_end_folder(self):
+        if not os.path.isdir(os.path.join(self.scanned_files_dir, 'icons_by_year')):
+            os.makedirs(os.path.join(self.scanned_files_dir, 'icons_by_year'))
+
+    def sort_files(self):
+        for dirpath, dirnames, filenames in os.walk(self.dir_to_scan):
             for img in filenames:
                 img_path = os.path.join(dirpath, img)
                 time_of_creation = time.gmtime(os.path.getmtime(img_path))[0:2]
-                final_dir = os.path.join(scanned_files_dir, 'icons_by_year', str(time_of_creation[0]),
+                final_dir = os.path.join(self.scanned_files_dir, 'icons_by_year', str(time_of_creation[0]),
                                          str(time_of_creation[1]))
                 if not os.path.isdir(final_dir):
                     os.makedirs(final_dir)
                 shutil.copy2(img_path, final_dir)
 
+    def sort(self):
+        self.create_end_folder()
+        self.sort_files()
 
-icons_folder = os.path.normpath('C:/Users/igorek/PycharmProjects/python_base/lesson_009/icons')
-# TODO В windows разве такие слеши в пути?
-final_folder = os.path.normpath('C:\\Users\igorek\PycharmProjects\python_base\lesson_009')
-# TOdO Константы
 
-a = PhotosSorter()
-a.sort_files(icons_folder, final_folder)
+class PhotosSorterZip(PhotosSorter):
+
+    def __init__(self, scanned_files_dir, dir_to_scan):
+        super().__init__(scanned_files_dir, dir_to_scan)
+
+    def zip_namelist(self):
+        with zp.ZipFile(self.dir_to_scan, 'r') as myzip:
+            return myzip.namelist()
+
+    def get_date(self, file_name):
+        with zp.ZipFile(self.dir_to_scan, 'r') as myzip:
+            return myzip.getinfo(file_name).date_time
+
+    def get_obj_info(self, file_name):
+        with zp.ZipFile(self.dir_to_scan, 'r') as myzip:
+            return myzip.getinfo(file_name)
+
+    def check_folder_existence(self, final_dir):
+        if not os.path.isdir(final_dir):
+            os.makedirs(final_dir)
+
+    def extract_image(self, filename, final_dir):
+        with zp.ZipFile(self.dir_to_scan, 'r') as myzip:
+            return myzip.extract(member=filename, path=final_dir)
+
+    def sort_files(self):
+        for filename in self.zip_namelist():
+            if filename[-3:] == 'png':
+                date = self.get_date(filename)
+                final_dir = os.path.join(self.scanned_files_dir, 'icons_by_year',
+                                         str(date[0]), str(date[1]))
+                self.check_folder_existence(final_dir)
+                self.extract_image(filename, final_dir)
+                break
+
+    def sort(self):
+        self.create_end_folder()
+        self.sort_files()
+
+
+a = PhotosSorterZip(FINAL_FOLDER, ICONS_FOLDER)
+a.sort_files()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
