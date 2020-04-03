6861d65e7a7f86fa110a9524750184b8087206ce
Igor Zhukov <igorek993@gmail.com>
2020-04-03 16:31:24 +1100

    lesson 10 tasks 1 and 2

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
-
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+def wired_function():
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
@@ -15,6 +17,11 @@ print(f"- Leeloo Dallas! Multi-pass № {result}!")
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
 
-
-
-
+try:
+    wired_function()
+except ValueError:
+    print('невозможно преобразовать к числу')
+except IndexError:
+    print('выход за границы списка')
+except BaseException as exp:
+    print(f'что то пошло не так{exp}')
--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import random
+from termcolor import cprint
+
 # День сурка
 #
 # Напишите функцию one_day() которая возвращает количество кармы от 1 до 7
@@ -19,6 +22,77 @@
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
 
-# https://goo.gl/JnsDqu
+class IamGodError(Exception):
+
+    def __init__(self):
+        self.message = 'Thought he was God today'
+
+    def __str__(self):
+        return self.message
+
+
+class DrunkError(Exception):
+
+    def __init__(self):
+        self.message = 'Got too drunk'
+
+    def __str__(self):
+        return self.message
+
+
+class CarCrashError(Exception):
+
+    def __init__(self):
+        self.message = 'Had a car crash'
+
+    def __str__(self):
+        return self.message
+
+
+class GluttonyError(Exception):
+
+    def __init__(self):
+        self.message = 'Overate'
+
+    def __str__(self):
+        return self.message
+
+
+class DepressionError(Exception):
+
+    def __init__(self):
+        self.message = 'Was in depression'
+
+    def __str__(self):
+        return self.message
+
+
+class SuicideError(Exception):
+
+    def __init__(self):
+        self.message = 'Tried to commit suicide'
+
+    def __str__(self):
+        return self.message
+
+
+exceptions_list = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+
+
+def one_day():
+    carma_upgrade = random.randint(1, 7)
+    if random.randint(1, 13) == 1:
+        raise exceptions_list[random.randint(0, 5)]
+    return carma_upgrade
+
+
+carma_level = 0
+day = 0
+while carma_level < ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        carma_level += one_day()
+        day += 1
+        print(f'day {day}, carma level {carma_level}')
+    except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exp:
+        cprint(exp, color='yellow')
