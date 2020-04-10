31999d792711acfb0a83e5e6c711a3e159d56cd4
Igor Zhukov <igorek993@gmail.com>
2020-04-10 16:25:48 +1000

    lesson 11 task 2 part 1 done

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-
 # Есть функция генерации списка простых чисел
 
 
 def get_prime_numbers(n):
     prime_numbers = []
-    for number in range(2, n+1):
+    for number in range(2, n + 1):
         for prime in prime_numbers:
             if number % prime == 0:
                 break
         else:
             prime_numbers.append(number)
     return prime_numbers
+
 
 # Часть 1
 # На основе алгоритма get_prime_numbers создать класс итерируемых обьектов,
@@ -22,8 +22,24 @@ def get_prime_numbers(n):
 
 
 class PrimeNumbers:
-    pass
-    # TODO здесь ваш код
+
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        self.prime_numbers = []
+        return self
+
+    def __next__(self):
+        for number in range(2, self.n + 1):
+            for prime in self.prime_numbers:
+                if number >= self.n:
+                    raise StopIteration()
+                if number % prime == 0:
+                    break
+            else:
+                self.prime_numbers.append(number)
+                return number
 
 
 prime_number_iterator = PrimeNumbers(n=10000)
@@ -31,7 +47,6 @@ for number in prime_number_iterator:
     print(number)
 
 
-# TODO после подтверждения части 1 преподователем, можно делать
 # Часть 2
 # Теперь нужно создать генератор, который выдает последовательность простых чисел до n
 # Распечатать все простые числа до 10000 в столбик
@@ -44,7 +59,6 @@ def prime_numbers_generator(n):
 
 for number in prime_numbers_generator(n=10000):
     print(number)
-
 
 # Часть 3
 # Написать несколько функций-фильтров, которые выдает True, если число:
