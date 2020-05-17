3bad913417f1c43a8e7bc64b23743c3e9c6cd95a
Igor Zhukov <igorek993@gmail.com>
2020-05-17 12:48:13 +1000

    lesson 14 state pattern I am really not sure what I have to do now

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -28,39 +28,91 @@ class EvenNumber(Exception):
         return self.message
 
 
-def get_score(game_result):
-    allowed_symbols = ['/', 'X', '-']
-    current_pair = str()
-    score = 0
-    for symbol in game_result:
-        if not symbol.isnumeric() and symbol not in allowed_symbols:
-            raise InvalidSymbol
-        elif current_pair and symbol == 'X':
-            raise EvenNumber
-        elif symbol == 'X':
-            score += 20
-            continue
-        elif current_pair:
-            current_pair += symbol
-            if symbol == '/':
-                score += 15
+# def get_score(game_result):
+#     allowed_symbols = ['/', 'X', '-']
+#     current_pair = str()
+#     score = 0
+#     for symbol in game_result:
+#         if not symbol.isnumeric() and symbol not in allowed_symbols:
+#             raise InvalidSymbol
+#         elif current_pair and symbol == 'X':
+#             raise EvenNumber
+#         elif symbol == 'X':
+#             score += 20
+#             continue
+#         elif current_pair:
+#             current_pair += symbol
+#             if symbol == '/':
+#                 score += 15
+#                 current_pair = str()
+#                 continue
+#             for i in current_pair:
+#                 if i == '-':
+#                     current_pair = current_pair.replace('-', '0')
+#             if int(current_pair[0]) + int(current_pair[1]) <= 9:
+#                 score += int(current_pair[0]) + int(current_pair[1])
+#             else:
+#                 raise MoreThan10
+#             current_pair = str()
+#         else:
+#             current_pair += symbol
+#     return score
+#
+#
+# if __name__ == "__main__":
+#     import doctest
+#
+#     doctest.testmod()
+
+
+class Manager:
+    def get_score(self, symbol):
+        allowed_symbols = ['/', 'X', '-']
+        score = 0
+        current_pair = str()
+        for symbol in symbol:
+            if not symbol.isnumeric() and symbol not in allowed_symbols:
+                raise InvalidSymbol
+            elif current_pair and symbol == 'X':
+                raise EvenNumber
+            elif symbol == 'X':
+                return 20
+            elif current_pair:
+                current_pair += symbol
+                if symbol == '/':
+                    score += 15
+                    current_pair = str()
+                    continue
+                for i in current_pair:
+                    if i == '-':
+                        current_pair = current_pair.replace('-', '0')
+                if int(current_pair[0]) + int(current_pair[1]) <= 9:
+                    score += int(current_pair[0]) + int(current_pair[1])
+                else:
+                    raise MoreThan10
                 current_pair = str()
-                continue
-            for i in current_pair:
-                if i == '-':
-                    current_pair = current_pair.replace('-', '0')
-            if int(current_pair[0]) + int(current_pair[1]) <= 10:  # todo Тут на деле не должно быть более 9, так как
-                                                                   #  если все кегли сбиты, то это spare - /
-                score += int(current_pair[0]) + int(current_pair[1])
             else:
-                raise MoreThan10
-            current_pair = str()
-        else:
-            current_pair += symbol
-    return score
+                current_pair += symbol
+        return score
 
 
-if __name__ == "__main__":
-    import doctest
+class ShotOne(Manager):
 
-    doctest.testmod()
+    def __init__(self):
+        pass
+
+
+class ShotTwo(Manager):
+
+    def __init__(self):
+        pass
+
+
+def bowling(result):
+    current_state, score = ShotOne(), 0
+    for symbol in result:
+        current_state, points = current_state.get_score(symbol)
+        score += points
+
+
+print(bowling('X4/34-4'))
